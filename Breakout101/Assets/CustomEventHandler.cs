03f74e36c4bdfd07575831694ff0bfe61588bb8b
JanaH <jana.helgath@stud.uni-regensburg.de>
2014-07-07 13:09:02 +0200

    added breakout

---
+++ Breakout101/Assets/CustomEventHandler.cs
@@ -0,0 +1,89 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CustomEventHandler : MonoBehaviour, ITrackableEventHandler
+{
+
+    private TrackableBehaviour mTrackableBehaviour, ITrackableEventHandler;
+    private bool isRenderer = false;
+    private GameObject ball;
+	// Use this for initialization
+    void Start()
+    {
+        ball = GameObject.Find("Ball");
+        mTrackableBehaviour = GetComponent<TrackableBehaviour>();
+        if (mTrackableBehaviour)
+        {
+            mTrackableBehaviour.RegisterTrackableEventHandler(this);
+        }
+        OnTrackingLost();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (isRenderer)
+        {
+            //ball.transform.position -= ball.transform.up * 5.0f * Time.deltaTime;
+            ball.AddComponent<Rigidbody>();
+        }
+	}
+
+    private void OnTrackingFound()
+    {
+        Renderer[] rendererComponents = GetComponentsInChildren<Renderer>(true);
+        Collider[] colliderComponents = GetComponentsInChildren<Collider>(true);
+
+        // Enable rendering:
+        foreach (Renderer component in rendererComponents)
+        {
+            component.enabled = true;
+        }
+
+        // Enable colliders:
+        foreach (Collider component in colliderComponents)
+        {
+            component.enabled = true;
+        }
+
+        Debug.Log("Trackable " + mTrackableBehaviour.TrackableName + " found");
+
+        isRenderer = true;
+    }
+
+    private void OnTrackingLost()
+    {
+        Renderer[] rendererComponents = GetComponentsInChildren<Renderer>(true);
+        Collider[] colliderComponents = GetComponentsInChildren<Collider>(true);
+
+        // Disable rendering:
+        foreach (Renderer component in rendererComponents)
+        {
+            component.enabled = false;
+        }
+
+        // Disable colliders:
+        foreach (Collider component in colliderComponents)
+        {
+            component.enabled = false;
+        }
+
+        Debug.Log("Trackable " + mTrackableBehaviour.TrackableName + " lost");
+        isRenderer = false;
+    }
+
+    public void OnTrackableStateChanged(
+                                    TrackableBehaviour.Status previousStatus,
+                                    TrackableBehaviour.Status newStatus)
+    {
+        if (newStatus == TrackableBehaviour.Status.DETECTED ||
+            newStatus == TrackableBehaviour.Status.TRACKED ||
+            newStatus == TrackableBehaviour.Status.EXTENDED_TRACKED)
+        {
+            OnTrackingFound();
+        }
+        else
+        {
+            OnTrackingLost();
+        }
+    }
+}
---
+++ Breakout101/Assets/DragObject.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DragObject : MonoBehaviour
+{
+    private float dist;
+    private Vector3 v3Offset;
+    private Plane plane;
+
+    void OnMouseDown()
+    {
+        plane.SetNormalAndPosition(Camera.main.transform.forward, transform.position);
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        float dist;
+        plane.Raycast(ray, out dist);
+        v3Offset = transform.position - ray.GetPoint(dist);
+    }
+
+    void OnMouseDrag()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        float dist;
+        plane.Raycast(ray, out dist);
+        Vector3 v3Pos = ray.GetPoint(dist);
+        transform.position = v3Pos + v3Offset;
+    }
+}
